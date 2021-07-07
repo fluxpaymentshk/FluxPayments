fee44a0d4d29faa862fb95b774226eaa8ed9f0dd
Monik09 <monikinderjitsingh5@gmail.com>
2021-07-07 17:22:32 +0530

    added lambdas functionality

--- lib/services/database_lambda.dart
+++ lib/services/database_lambda.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:aws_lambda/aws_lambda.dart';
+import 'package:flutter/foundation.dart';
 
 class DatabaseLambdaService {
   Map<String, dynamic> result = {};
@@ -13,12 +16,63 @@ class DatabaseLambdaService {
         });
   }
 
-  Future<Map<String, dynamic>> test() async {
+  Future<Map<String, dynamic>> updateFluxPoints({@required String? userID,@required String? appEvent,@required int? servicePoints}) async {
     result = {};
     try {
-      result = await lambda
-          .callLambda('aurora-serverless-function-referral', <String, dynamic>{
-        'key1': ["1234vidd", "flux-vid3"],
+      result = await lambda.callLambda(
+          'aurora-serverless-function-FluxPoints', <String, dynamic>{
+        "userID": userID,
+        "appEvent": appEvent,
+        "servicePoints": servicePoints
+      }).then((value) {
+        log("$value");
+        return value;
+      });
+      print(
+          "---------------------------------------------------------------------------------$result");
+    } catch (e) {
+      print(e);
+    }
+    return result;
+  }
+
+  Future<Map<String, dynamic>> getUserBillProviderDetails({@required String? userID}) async {
+    result = {};
+    try {
+      result = await lambda.callLambda(
+          'aurora-serverless-function-UserBillProvider', <String, dynamic>{
+        "userID": userID,
+      });
+      print(
+          "---------------------------------------------------------------------------------$result");
+    } catch (e) {
+      print(e);
+    }
+    return result;
+  }
+
+  Future<Map<String, dynamic>> getUserRewardDetails({@required String? userID}) async {
+    result = {};
+    try {
+      result = await lambda.callLambda(
+          'aurora-serverless-function-UserRewards', <String, dynamic>{
+        "userID": userID,
+      });
+      print(
+          "---------------------------------------------------------------------------------$result");
+    } catch (e) {
+      print(e);
+    }
+    return result;
+  }
+  
+  Future<Map<String, dynamic>> getSingleTransactionDetails({@required String? userID,@required String? serviceTransactionID}) async {
+    result = {};
+    try {
+      result = await lambda.callLambda(
+          'aurora-serverless-function-SingleTransactionDetail', <String, dynamic>{
+        "userID": userID,
+        "serviceTransactionID":serviceTransactionID
       });
       print(
           "---------------------------------------------------------------------------------$result");
